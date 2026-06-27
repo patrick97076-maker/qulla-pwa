627518370a5c838b361c878a54c173c6dbed0336
patrick97076-maker <patrick97076@googlemail.com>
2026-06-27 21:55:21 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("q-cache").then(cache => {
+      return cache.addAll(["/", "/index.html", "/app.js", "/manifest.json"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
